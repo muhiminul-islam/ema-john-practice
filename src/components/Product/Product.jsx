d13ea1a3cff8fddccf8b5b066a17af5ab5a66ade
muhiminul-islam <islammuhiminul11@gmail.com>
2023-03-28 02:28:46 +0600

    eventHandlerAdd done in parent component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const { img, name, seller, ratings, price } = props.product;
+    const { img, name, seller, ratings, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+    
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +14,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings}</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,11 +4,18 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+
     useEffect( () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
+
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -16,6 +23,7 @@ const Shop = () => {
                     products.map(product => <Product 
                         key = {product.id}
                         product = {product}
+                        handleAddToCart = {handleAddToCart}
                     ></Product>)
                 }
             </div>
