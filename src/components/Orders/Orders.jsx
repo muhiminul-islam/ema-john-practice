dde636aa50a6af65017a459e657a514077835d2d
muhiminul-islam <islammuhiminul11@gmail.com>
2023-04-04 23:45:40 +0600

    cart-item container shared two pages

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const cart = useLoaderData();
+    console.log(cart);
+
     return (
-        <div>
-            <h1>Orders Page</h1>
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h3>Orders Page: {cart.length}</h3>
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
